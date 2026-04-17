601a3739ed9a7ed09983f56bc9c06f77fc5ad47f
Brandon Katz <brandon@katz.co.za>
2026-04-17 16:25:29 +0200

    Auto: Add 10 churches (Alberton, Germiston, Brakpan, Springs, Tembisa, Pinetown, Bellville, Worcester)

---
+++ seed-2026-04-17.sql
@@ -0,0 +1,32 @@
+-- ChurchesNearMe: 10 new churches added 2026-04-17
+-- Focus: Gauteng (Alberton, Germiston, Brakpan, Springs, Tembisa), KZN (Pinetown, Richards Bay), WC (Bellville, Worcester)
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('NG Kerk Alberton', 'Dutch Reformed', 'Voortrekker Rd, Alberton', 'Alberton', 'Gauteng', -26.2655, 28.1230, NULL, '011 869 1234', 'ng-kerk-alberton', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Alberton Baptist Church', 'Baptist', 'New Redruth Ave, Alberton', 'Alberton', 'Gauteng', -26.2680, 28.1255, NULL, NULL, 'alberton-baptist-church', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('NG Kerk Germiston', 'Dutch Reformed', 'Webber St, Germiston', 'Germiston', 'Gauteng', -26.2200, 28.1680, NULL, '011 827 2100', 'ng-kerk-germiston', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Rhema Germiston', 'Rhema', 'Elandsfontein Rd, Germiston', 'Germiston', 'Gauteng', -26.2225, 28.1710, 'https://rhema.co.za', NULL, 'rhema-germiston', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('AFM Brakpan', 'Apostolic Faith Mission', 'Voortrekker Rd, Brakpan', 'Brakpan', 'Gauteng', -26.2355, 28.3690, 'https://afm-ags.org', NULL, 'afm-brakpan', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Springs City Church', 'Charismatic', 'Geduld Rd, Springs', 'Springs', 'Gauteng', -26.2480, 28.4420, NULL, '011 812 5500', 'springs-city-church', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('AFM Tembisa', 'Apostolic Faith Mission', 'Umthambeka Rd, Tembisa', 'Tembisa', 'Gauteng', -26.0000, 28.2250, 'https://afm-ags.org', NULL, 'afm-tembisa', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Pinetown Christian Church', 'Charismatic', 'Old Main Rd, Pinetown', 'Pinetown', 'KwaZulu-Natal', -29.8168, 30.8610, NULL, '031 702 1234', 'pinetown-christian-church', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Bellville Community Church', 'Community Church', 'Durban Rd, Bellville', 'Bellville', 'Western Cape', -33.8985, 18.6290, NULL, '021 948 5678', 'bellville-community-church', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Worcester Baptist Church', 'Baptist', 'Church St, Worcester', 'Worcester', 'Western Cape', -33.6450, 19.4415, NULL, '023 342 1234', 'worcester-baptist-church', TRUE, FALSE);
