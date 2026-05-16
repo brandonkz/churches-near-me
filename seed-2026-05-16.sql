e2cc5fa91cca4ca85d68db57e817e289cc5c6ba0
Brandon Katz <brandon@katz.co.za>
2026-05-16 03:13:09 +0200

    Auto: Add new churches

---
+++ seed-2026-05-16.sql
@@ -0,0 +1,26 @@
+-- ChurchesNearMe: 8 new churches added 2026-05-16
+-- Focus: Gauteng, KwaZulu-Natal, Western Cape
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('New Covenant Baptist Church', 'Baptist', 'Randburg', 'Johannesburg', 'Gauteng', -26.1000, 27.9900, 'https://ncbc.co.za/', NULL, 'new-covenant-baptist-church-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Gracepoint Methodist Church', 'Methodist', 'Glenferness', 'Johannesburg', 'Gauteng', -25.9970, 28.0070, 'https://www.gracepoint.co.za/', NULL, 'gracepoint-methodist-church-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Durban North Community Church', 'Community Church', 'Durban North', 'Durban', 'KwaZulu-Natal', -29.7840, 31.0370, 'https://dncc.org.za/', NULL, 'durban-north-community-church', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Kloof Harvest Church', 'Charismatic', 'Kloof', 'Durban', 'KwaZulu-Natal', -29.7850, 30.8320, 'https://www.kloofharvest.co.za/', NULL, 'kloof-harvest-church-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('CityHill Church Hillcrest', 'Charismatic', 'Hillcrest', 'Durban', 'KwaZulu-Natal', -29.7760, 30.7630, 'https://cityhillchurch.com/', NULL, 'cityhill-church-hillcrest-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('St Andrew''s Presbyterian Church Cape Town', 'Presbyterian', 'Somerset Road, Cape Town', 'Cape Town', 'Western Cape', -33.9150, 18.4170, 'https://standrews1828.co.za/', '021 419 1059', 'st-andrews-presbyterian-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Urban Edge Church Durbanville', 'Non-Denominational', 'Durbanville', 'Cape Town', 'Western Cape', -33.8320, 18.6470, 'https://urbanedge.org.za/', NULL, 'urban-edge-church-durbanville-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Joshua Generation Church Sunningdale', 'Non-Denominational', 'Sunningdale', 'Cape Town', 'Western Cape', -33.8030, 18.4850, 'https://joshuageneration.org/', NULL, 'joshua-generation-church-sunningdale-cape-town', TRUE, FALSE);
