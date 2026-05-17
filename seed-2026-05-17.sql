1e7e85fe5f17c874888aba6d593e3b66819cf19b
Brandon Katz <brandon@katz.co.za>
2026-05-17 03:13:03 +0200

    Auto: Add new churches

---
+++ seed-2026-05-17.sql
@@ -0,0 +1,32 @@
+-- ChurchesNearMe: 10 new churches added 2026-05-17
+-- Focus: Gauteng, KwaZulu-Natal, Western Cape
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Mosaiek Church', 'Non-Denominational', 'Fairland, Randburg', 'Johannesburg', 'Gauteng', -26.1380, 27.9520, 'https://mosaiek.com/', NULL, 'mosaiek-church-randburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Rosebank Catholic Church', 'Catholic', 'Rosebank', 'Johannesburg', 'Gauteng', -26.1460, 28.0420, 'https://rosebankcatholicchurch.org.za/', NULL, 'rosebank-catholic-church-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('St John''s Parish Wynberg', 'Anglican', 'Wynberg', 'Cape Town', 'Western Cape', -34.0060, 18.4680, 'https://stjohns-wynberg.org.za/', NULL, 'st-johns-parish-wynberg-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Common Ground Constantiaberg', 'Non-Denominational', 'Constantiaberg', 'Cape Town', 'Western Cape', -34.0350, 18.4410, 'https://commonground.co.za/', NULL, 'commonground-constantiaberg-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Newfrontiers Church Durbanville', 'Non-Denominational', 'Durbanville', 'Cape Town', 'Western Cape', -33.8300, 18.6490, 'https://newfrontierschurch.co.za/', NULL, 'newfrontiers-church-durbanville-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Open Skies Church', 'Non-Denominational', 'Durban North', 'Durban', 'KwaZulu-Natal', -29.7890, 31.0320, 'https://openskieschurch.com/', NULL, 'open-skies-church-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Kingsway Church', 'Non-Denominational', 'Pinetown', 'Durban', 'KwaZulu-Natal', -29.8180, 30.8570, 'https://kingswaychurch.co.za/', NULL, 'kingsway-church-pinetown', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Westville Catholic Church', 'Catholic', 'Westville', 'Durban', 'KwaZulu-Natal', -29.8320, 30.9270, 'https://westvillecatholicchurch.co.za/', NULL, 'westville-catholic-church-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('St Thomas More Catholic Church Durban North', 'Catholic', 'Durban North', 'Durban', 'KwaZulu-Natal', -29.7830, 31.0410, 'https://stmorercchurch.co.za/', NULL, 'st-thomas-more-catholic-church-durban-north', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Northfield Methodist Church', 'Methodist', 'Benoni', 'Johannesburg', 'Gauteng', -26.1770, 28.3080, 'https://northfield.org.za/', NULL, 'northfield-methodist-church-benoni', TRUE, FALSE);
