8c6da9fc021d00b8fc28428b36bd4c733b966fe8
Brandon Katz <brandon@katz.co.za>
2026-05-18 03:13:48 +0200

    Auto: Add new churches

---
+++ seed-2026-05-18.sql
@@ -0,0 +1,26 @@
+-- ChurchesNearMe: 8 new churches added 2026-05-18
+-- Focus: Gauteng, KwaZulu-Natal, Western Cape
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Bryanston Bible Church', 'Evangelical', 'Bryanston', 'Johannesburg', 'Gauteng', -26.0520, 28.0240, 'https://www.bryanstonbiblechurch.org.za/', NULL, 'bryanston-bible-church-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Northcliff Union Church', 'United Church', 'Northcliff', 'Johannesburg', 'Gauteng', -26.1450, 27.9680, 'https://www.nuc.co.za/', NULL, 'northcliff-union-church-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('St Agnes Anglican Church Kloof', 'Anglican', 'Kloof', 'Durban', 'KwaZulu-Natal', -29.7860, 30.8350, 'https://stagnes.org.za/', NULL, 'st-agnes-anglican-church-kloof', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('View Church Table View', 'Non-Denominational', 'Table View', 'Cape Town', 'Western Cape', -33.8250, 18.4940, 'https://viewchurch.co.za/', NULL, 'view-church-table-view-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Glenridge Church', 'Non-Denominational', 'Durban North', 'Durban', 'KwaZulu-Natal', -29.7830, 31.0370, 'https://glenridge.org.za/', NULL, 'glenridge-church-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Bergvliet Methodist Church', 'Methodist', 'Bergvliet', 'Cape Town', 'Western Cape', -34.0470, 18.4520, 'https://bergvlietmethodist.co.za/', NULL, 'bergvliet-methodist-church-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('St Saviour''s Church Claremont', 'Anglican', 'Claremont', 'Cape Town', 'Western Cape', -33.9810, 18.4650, 'https://stsaviours.org.za/', NULL, 'st-saviours-church-claremont-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Durbanville Methodist Church', 'Methodist', 'Durbanville', 'Cape Town', 'Western Cape', -33.8300, 18.6490, 'https://durbanvillemethodist.co.za/', NULL, 'durbanville-methodist-church-cape-town', TRUE, FALSE);
