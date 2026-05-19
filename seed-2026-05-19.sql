55ae38b87caaa0bf03d2bbc5c789dae61596ca9b
Brandon Katz <brandon@katz.co.za>
2026-05-19 03:13:12 +0200

    Auto: Add new churches

---
+++ seed-2026-05-19.sql
@@ -0,0 +1,26 @@
+-- ChurchesNearMe: 8 new churches added 2026-05-19
+-- Focus: Gauteng, KwaZulu-Natal, Western Cape
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('St Martin''s in-the-Veld Anglican Church', 'Anglican', '43 Cradock Avenue, Rosebank', 'Johannesburg', 'Gauteng', -26.1460, 28.0380, 'https://www.stmartins.co.za/', NULL, 'st-martins-in-the-veld-anglican-rosebank-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('The Barn Christian Fellowship', 'Non-Denominational', 'Kelly Avenue and Sylvan Road, Boskruin', 'Johannesburg', 'Gauteng', -26.0960, 27.9580, 'https://www.thebarn.co.za/', NULL, 'the-barn-christian-fellowship-randburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('GodFirst Church East Rand', 'Non-Denominational', 'Northvilla Terrace, Northvilla, Benoni', 'Benoni', 'Gauteng', -26.1670, 28.3260, 'https://www.g1eastrand.co.za/', NULL, 'godfirst-church-east-rand-benoni', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Highway Christian Community', 'Pentecostal', '10 Harvey Road, Pinetown', 'Pinetown', 'KwaZulu-Natal', -29.8160, 30.8610, 'https://highway.org.za/', NULL, 'highway-christian-community-pinetown', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('St Elizabeth''s Anglican Church Westville', 'Anglican', 'Westville', 'Durban', 'KwaZulu-Natal', -29.8310, 30.9210, 'http://www.stelizabeths.org.za/', NULL, 'st-elizabeths-anglican-westville-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Life Changers Church Milnerton', 'Non-Denominational', 'Milnerton', 'Cape Town', 'Western Cape', -33.8740, 18.5050, 'https://lifechangerschurch.co.za/', NULL, 'life-changers-church-milnerton-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Church on Main Wynberg', 'Non-Denominational', '3 Millbank Road, Wynberg', 'Cape Town', 'Western Cape', -34.0050, 18.4680, 'https://churchonmain.org.za/', NULL, 'church-on-main-wynberg-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('The Bay Christian Family Church Muizenberg', 'Non-Denominational', 'Muizenberg', 'Cape Town', 'Western Cape', -34.1080, 18.4700, 'https://thebaycfc.org.za/', NULL, 'the-bay-christian-family-church-muizenberg-cape-town', TRUE, FALSE);
