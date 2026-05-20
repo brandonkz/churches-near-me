54258d26604f31e535fd178581d5666593277742
Brandon Katz <brandon@katz.co.za>
2026-05-20 03:15:53 +0200

    Auto: Add new churches

---
+++ seed-2026-05-20.sql
@@ -0,0 +1,23 @@
+-- ChurchesNearMe: 8 new churches added 2026-05-20
+-- Focus: Gauteng, KwaZulu-Natal, Western Cape
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('St Luke''s Anglican Church Orchards', 'Anglican', 'High Road, Orchards', 'Johannesburg', 'Gauteng', -26.1530, 28.0760, 'https://stlukes.org.za/', NULL, 'st-lukes-anglican-orchards-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Melrose Methodist Church', 'Methodist', 'Melrose', 'Johannesburg', 'Gauteng', -26.1350, 28.0670, 'https://melrosemethodist.org.za/', NULL, 'melrose-methodist-church-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('St Thomas Anglican Church Musgrave', 'Anglican', 'Musgrave Road, Musgrave', 'Durban', 'KwaZulu-Natal', -29.8460, 30.9990, 'https://stthomasmusgrave.co.za/', NULL, 'st-thomas-anglican-musgrave-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Glenwood Presbyterian Church', 'Presbyterian', 'Glenwood', 'Durban', 'KwaZulu-Natal', -29.8610, 30.9970, 'https://glenwoodpresbyterian.co.za/', NULL, 'glenwood-presbyterian-church-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Kearsney College Chapel', 'Methodist', '25 Old Main Road, Botha''s Hill', 'Durban', 'KwaZulu-Natal', -29.7370, 30.7710, 'https://www.kearsney.com/', NULL, 'kearsney-college-chapel-bothas-hill-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('St Michael and All Angels Observatory', 'Anglican', 'Observatory', 'Cape Town', 'Western Cape', -33.9360, 18.4710, 'https://stmichaels.org.za/', NULL, 'st-michael-and-all-angels-observatory-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('St Peter''s Anglican Church Mowbray', 'Anglican', 'Mowbray', 'Cape Town', 'Western Cape', -33.9460, 18.4760, 'https://stpetersmowbray.org.za/', NULL, 'st-peters-anglican-mowbray-cape-town', TRUE, FALSE);
