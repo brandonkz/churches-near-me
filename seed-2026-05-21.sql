acad323baca1d6b7338d1b09f829b21fc9d7b000
Brandon Katz <brandon@katz.co.za>
2026-05-21 03:14:24 +0200

    Auto: Add new churches

---
+++ seed-2026-05-21.sql
@@ -0,0 +1,23 @@
+-- ChurchesNearMe: 7 new churches added 2026-05-21
+-- Focus: Gauteng, KwaZulu-Natal, Western Cape
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('St Faith''s Church Plumstead', 'Anglican', 'Plumstead', 'Cape Town', 'Western Cape', -34.0190, 18.4680, 'https://www.stfaithscentre.org/', NULL, 'st-faiths-church-plumstead-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('St Cyprian''s Church Retreat', 'Anglican', 'Retreat', 'Cape Town', 'Western Cape', -34.0560, 18.4720, 'https://www.facebook.com/stcyprianschurch/', NULL, 'st-cyprians-church-retreat-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('All Saints Anglican Church Belhar', 'Anglican', '1 Denneboom Road, Belhar', 'Cape Town', 'Western Cape', -33.9460, 18.6320, NULL, '021 952 4762', 'all-saints-anglican-belhar-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('St Francis of Assisi Parkview', 'Anglican', '46 Tyrone Avenue, Parkview', 'Johannesburg', 'Gauteng', -26.1630, 28.0300, 'https://stfrancisparkview.com/', '011 646 2660', 'st-francis-of-assisi-parkview-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('St Mungo''s United Church Bryanston', 'United Church', 'William Nicol Drive and Grosvenor Road, Bryanston', 'Johannesburg', 'Gauteng', -26.0530, 28.0210, 'http://stmungo.co.za/', NULL, 'st-mungos-united-church-bryanston-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Umhlali Methodist Church', 'Methodist', 'Umhlali', 'Ballito', 'KwaZulu-Natal', -29.4710, 31.2180, 'https://www.umc.org.za/', NULL, 'umhlali-methodist-church-ballito', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, slug, verified, featured) VALUES
+('Amanzimtoti Methodist Church', 'Methodist', '1 Dingwall Road, Amanzimtoti', 'Durban', 'KwaZulu-Natal', -30.0490, 30.8850, NULL, NULL, 'amanzimtoti-methodist-church-durban', TRUE, FALSE);
