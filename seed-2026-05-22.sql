e5a5272215373618c336f2b3628daf1d61bec0fc
Brandon Katz <brandon@katz.co.za>
2026-05-22 03:15:15 +0200

    Auto: Add new churches

---
+++ seed-2026-05-22.sql
@@ -0,0 +1,20 @@
+-- ChurchesNearMe: 6 new churches added 2026-05-22
+-- Focus: Gauteng, KwaZulu-Natal, Western Cape
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Church of the Resurrection Bryanston', 'Catholic', '18 Sloane Street, Bryanston', 'Johannesburg', 'Gauteng', -26.0520, 28.0260, 'https://catholicjhb.org.za/directory/listing/bryanston-church-of-the-resurrection-catholic-church-est-1968/', NULL, NULL, 'church-of-the-resurrection-bryanston-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Immaculate Conception Catholic Church Rosebank', 'Catholic', '16 Keyes Avenue, Rosebank', 'Johannesburg', 'Gauteng', -26.1440, 28.0360, 'https://catholicjhb.org.za/directory/listing/rosebank-immaculate-conception-catholic-church-est-1936/', NULL, NULL, 'immaculate-conception-catholic-rosebank-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Olive Tree Church Florida Road', 'Non-Denominational', 'Florida Road, Durban', 'Durban', 'KwaZulu-Natal', -29.8320, 31.0140, 'https://otc.org.za/sites/florida-road', NULL, NULL, 'olive-tree-church-florida-road-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St John''s Church Wynberg', 'Anglican', 'St John''s Road, Wynberg', 'Cape Town', 'Western Cape', -34.0060, 18.4660, 'https://www.stjohns.org.za/', NULL, NULL, 'st-johns-church-wynberg-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Thomas Rondebosch', 'Anglican', '61 Camp Ground Road, Rondebosch', 'Cape Town', 'Western Cape', -33.9630, 18.4730, 'https://str.org.za/', '021 685 6752', 'parishoffice@str.org.za', 'st-thomas-rondebosch-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Christ the King Catholic Church Pinelands', 'Catholic', 'Corner Union Avenue and Queens Way, Pinelands', 'Cape Town', 'Western Cape', -33.9340, 18.5090, 'https://adct.org.za/pinelands/', '021 531 7049', NULL, 'christ-the-king-catholic-church-pinelands-cape-town', TRUE, FALSE);
