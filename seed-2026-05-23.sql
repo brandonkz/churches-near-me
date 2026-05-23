c4ab73e726e788de7f25f30a7a56b2164e75b915
Brandon Katz <brandon@katz.co.za>
2026-05-23 03:14:59 +0200

    Auto: Add new churches

---
+++ seed-2026-05-23.sql
@@ -0,0 +1,26 @@
+-- ChurchesNearMe: 8 new churches added 2026-05-23
+-- Focus: Gauteng, KwaZulu-Natal, Western Cape
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Woodmead Halls Church', 'Evangelical', '2 Western Service Road, Woodmead', 'Johannesburg', 'Gauteng', -26.0450, 28.0880, 'https://woodmeadhalls.org.za/', NULL, NULL, 'woodmead-halls-church-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Christ Church Blairgowrie', 'Anglican', '34 Gordon Avenue, Blairgowrie', 'Johannesburg', 'Gauteng', -26.1225, 28.0130, 'https://christchurchblairgowrie.co.za/', NULL, NULL, 'christ-church-blairgowrie-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('The Hill Church', 'Non-Denominational', '66 Impala Road, Rispark', 'Johannesburg', 'Gauteng', -26.3020, 28.0350, 'https://thehillchurch.co.za/', NULL, NULL, 'the-hill-church-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Lewende Woord Brummeria', 'Charismatic', 'Brummeria, Pretoria East', 'Pretoria', 'Gauteng', -25.7430, 28.2760, 'https://lewenderwoord.co.za/', NULL, NULL, 'lewende-woord-brummeria-pretoria', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Pinetown Methodist Church', 'Methodist', '62 Bamboo Lane, Pinetown', 'Durban', 'KwaZulu-Natal', -29.8140, 30.8580, 'https://pinetownmethodist.co.za/', NULL, NULL, 'pinetown-methodist-church-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Westville Presbyterian Church', 'Presbyterian', 'Westville Road, Westville', 'Durban', 'KwaZulu-Natal', -29.8330, 30.9240, 'https://www.westvillepresby.co.za/', NULL, NULL, 'westville-presbyterian-church-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Stephen''s Anglican Church Pinelands', 'Anglican', 'Central Square, Pinelands', 'Cape Town', 'Western Cape', -33.9360, 18.5120, 'https://ststephenspinelands.org.za/', NULL, NULL, 'st-stephens-anglican-church-pinelands-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Fish Hoek Methodist Church', 'Methodist', '1st Crescent, Fish Hoek', 'Cape Town', 'Western Cape', -34.1360, 18.4320, 'https://fishhoekmethodist.co.za/', NULL, NULL, 'fish-hoek-methodist-church-cape-town', TRUE, FALSE);
