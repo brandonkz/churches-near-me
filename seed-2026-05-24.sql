ae1fdc1302db5e09e1de0a470ca874c1d9c17408
Brandon Katz <brandon@katz.co.za>
2026-05-24 03:15:05 +0200

    Auto: Add new churches

---
+++ seed-2026-05-24.sql
@@ -0,0 +1,32 @@
+-- ChurchesNearMe: 10 new churches added 2026-05-24
+-- Focus: Gauteng, KwaZulu-Natal, Western Cape
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Melville Methodist Church', 'Methodist', 'Corner 4th Avenue and 4th Street, Melville', 'Johannesburg', 'Gauteng', -26.1759, 28.0085, 'https://melvillemethodist.wordpress.com/', '082 077 1494', NULL, 'melville-methodist-church-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Linden Baptist Church', 'Baptist', 'Linden, Johannesburg', 'Johannesburg', 'Gauteng', -26.1350, 27.9920, 'https://lindenbaptist.org/', NULL, NULL, 'linden-baptist-church-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Linden Presbyterian Church', 'Presbyterian', '5th Avenue, Franklin Roosevelt Park', 'Johannesburg', 'Gauteng', -26.1430, 27.9870, 'https://www.lindenpc.org/', NULL, NULL, 'linden-presbyterian-church-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Margaret''s Anglican Church Bedfordview', 'Anglican', 'Bedfordview', 'Johannesburg', 'Gauteng', -26.1780, 28.1350, 'https://www.stmargarets.org.za/', NULL, NULL, 'st-margarets-anglican-bedfordview-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Bedfordview Methodist Church', 'Methodist', 'Corner Van Buuren Road and De Wet Street, Bedfordview', 'Johannesburg', 'Gauteng', -26.1810, 28.1370, 'https://bedfordviewmc.co.za/', '011 455 2600', NULL, 'bedfordview-methodist-church-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Stephen''s Anglican Church Sunninghill', 'Anglican', '67 Nanyuki Road, Sunninghill', 'Johannesburg', 'Gauteng', -26.0340, 28.0730, 'https://ststephenssa.co.za/', NULL, NULL, 'st-stephens-anglican-sunninghill-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Montclair Methodist Church', 'Methodist', '82 Montclair Road, Montclair', 'Durban', 'KwaZulu-Natal', -29.9180, 30.9530, 'https://www.facebook.com/MCSAMontclair/', NULL, NULL, 'montclair-methodist-church-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Bluff Methodist Church', 'Methodist', 'Wesley Road, Bluff', 'Durban', 'KwaZulu-Natal', -29.9210, 31.0000, 'https://www.facebook.com/BluffMethodist/', NULL, NULL, 'bluff-methodist-church-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Oswald''s Anglican Church Milnerton', 'Anglican', '34 Park Avenue, Milnerton', 'Cape Town', 'Western Cape', -33.8660, 18.4970, 'https://www.stoswalds.org.za/', '021 552 3262', NULL, 'st-oswalds-anglican-church-milnerton-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Mark''s Anglican Church District Six', 'Anglican', 'District Six', 'Cape Town', 'Western Cape', -33.9330, 18.4260, 'http://www.stmarksdistrict6.faithweb.com/', NULL, NULL, 'st-marks-anglican-church-district-six-cape-town', TRUE, FALSE);
