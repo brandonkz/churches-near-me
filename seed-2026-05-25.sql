6adcd669752e88f3dab4f1c46ac8ae3e6f4b003d
Brandon Katz <brandon@katz.co.za>
2026-05-25 03:15:04 +0200

    Auto: Add new churches

---
+++ seed-2026-05-25.sql
@@ -0,0 +1,29 @@
+-- ChurchesNearMe: 9 new churches added 2026-05-25
+-- Focus: Gauteng, KwaZulu-Natal, Western Cape
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Laurence''s Anglican Church Discovery', 'Anglican', 'Discovery, Roodepoort', 'Roodepoort', 'Gauteng', -26.1540, 27.8940, 'https://threesaints.co.za/st-laurence-s', NULL, NULL, 'st-laurences-anglican-discovery-roodepoort', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Michael and All Angels Weltevreden Park', 'Anglican', 'Weltevreden Park', 'Johannesburg', 'Gauteng', -26.1250, 27.9220, 'https://www.youtube.com/c/StMichaelandAllAngelsWeltevredenPark', NULL, NULL, 'st-michael-and-all-angels-weltevreden-park-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Mark''s Anglican Church North Riding', 'Anglican', '57 Hyperion Road, North Riding', 'Johannesburg', 'Gauteng', -26.0480, 27.9430, 'https://southafricachurches.com/listings/st-marks-anglican-church-north-riding/', NULL, NULL, 'st-marks-anglican-church-north-riding-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Musgrave Methodist Church', 'Methodist', 'Musgrave', 'Durban', 'KwaZulu-Natal', -29.8490, 31.0000, 'https://musgravemethodist.co.za/', NULL, NULL, 'musgrave-methodist-church-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Cyprian''s Anglican Church Durban', 'Anglican', 'Durban', 'Durban', 'KwaZulu-Natal', -29.8587, 31.0218, 'https://www.facebook.com/saintcyprians/', NULL, NULL, 'st-cyprians-anglican-church-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St John''s Anglican Church Pinetown', 'Anglican', 'Pinetown', 'Pinetown', 'KwaZulu-Natal', -29.8190, 30.8570, NULL, NULL, NULL, 'st-johns-anglican-church-pinetown', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Martin''s Anglican Church Bergvliet', 'Anglican', 'Bergvliet', 'Cape Town', 'Western Cape', -34.0470, 18.4540, 'https://stmartinsbergvliet.weebly.com/', NULL, NULL, 'st-martins-anglican-church-bergvliet-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Philip''s Anglican Church Kenwyn', 'Anglican', 'Kenwyn', 'Cape Town', 'Western Cape', -33.9950, 18.4960, 'https://spk.org.za/', NULL, NULL, 'st-philips-anglican-church-kenwyn-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Kenilworth Methodist Church', 'Methodist', 'Kenilworth', 'Cape Town', 'Western Cape', -33.9940, 18.4740, 'https://community-services.blaauwberg.net/churches-western-cape/methodist-churches-western-cape/kenilworth-methodist-church', NULL, NULL, 'kenilworth-methodist-church-cape-town', TRUE, FALSE);
