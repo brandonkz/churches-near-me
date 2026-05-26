7a9748d274290f5bbb4dae1ef94d457510bdfd4c
Brandon Katz <brandon@katz.co.za>
2026-05-26 03:13:16 +0200

    Auto: Add new churches

---
+++ seed-2026-05-26.sql
@@ -0,0 +1,23 @@
+-- ChurchesNearMe: 7 new churches added 2026-05-26
+-- Focus: Gauteng, KwaZulu-Natal, Western Cape
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Benoni Central Methodist Church', 'Methodist', '26 Bunyan Street, Benoni', 'Benoni', 'Gauteng', -26.1880, 28.3200, 'https://benonicentralmethodistchurch.org.za/', NULL, NULL, 'benoni-central-methodist-church', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('The English Reformed Church', 'Presbyterian', 'Randburg', 'Johannesburg', 'Gauteng', -26.0960, 28.0040, 'https://www.erc.org.za/', NULL, NULL, 'the-english-reformed-church-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Martin''s Anglican Church Durban North', 'Anglican', '12 Chelsea Drive, Durban North', 'Durban', 'KwaZulu-Natal', -29.7820, 31.0410, 'https://www.worldanglican.com/south-africa/durban-north/the-anglican-church-of-southern-africa/-st-martin-durban-north', NULL, NULL, 'st-martins-anglican-church-durban-north', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Durban North Methodist Church', 'Methodist', '3 Swapo Road, Broadway', 'Durban', 'KwaZulu-Natal', -29.7830, 31.0370, 'https://www.brabys.com/za/kwazulu-natal/durban/methodist-church', NULL, NULL, 'durban-north-methodist-church', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Mowbray Presbyterian Church', 'Presbyterian', 'Mowbray', 'Cape Town', 'Western Cape', -33.9480, 18.4760, 'https://mowbraypresby.org.za/', NULL, NULL, 'mowbray-presbyterian-church-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Gardens Presbyterian Church', 'Presbyterian', '151 Hatfield Street, Gardens', 'Cape Town', 'Western Cape', -33.9290, 18.4150, 'https://unitingpresbyterian.org/congregations/findcongregations/locality/Cape%20Town/', NULL, NULL, 'gardens-presbyterian-church-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Rosebank Methodist Church Cape Town', 'Methodist', '2 Chapel Road, Rosebank', 'Cape Town', 'Western Cape', -33.9550, 18.4750, 'https://rosebankmethodist.org.za/', NULL, NULL, 'rosebank-methodist-church-cape-town', TRUE, FALSE);
