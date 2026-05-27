97cb0dc7bc00d24d163390b621748770ef1e25ec
Brandon Katz <brandon@katz.co.za>
2026-05-27 03:15:19 +0200

    Auto: Add new churches

---
+++ seed-2026-05-27.sql
@@ -0,0 +1,20 @@
+-- ChurchesNearMe: 6 new churches added 2026-05-27
+-- Focus: Gauteng, KwaZulu-Natal, Western Cape
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Emmanuel Presbyterian Church Pretoria', 'Presbyterian', 'Pretoria East', 'Pretoria', 'Gauteng', -25.7840, 28.3150, 'https://emmanuel.org.za/', NULL, NULL, 'emmanuel-presbyterian-church-pretoria', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Trinity Methodist Church Linden', 'Methodist', 'Linden', 'Johannesburg', 'Gauteng', -26.1370, 27.9920, 'https://www.trinitylinden.org.za/', NULL, NULL, 'trinity-methodist-church-linden-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Queensburgh Methodist Church', 'Methodist', 'Queensburgh', 'Durban', 'KwaZulu-Natal', -29.8740, 30.9180, 'https://www.qbmc.co.za/', NULL, NULL, 'queensburgh-methodist-church-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Berea Presbyterian Church Durban', 'Presbyterian', 'Berea', 'Durban', 'KwaZulu-Natal', -29.8480, 31.0130, 'https://www.genza.org.za/kerke-churches/KWAZULU-NATAL/KZN-DURBAN/Berea-Presbyterian-Church/', NULL, NULL, 'berea-presbyterian-church-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Bernard Catholic Church Newlands', 'Catholic', 'Corner Protea and Buchan Roads, Newlands', 'Cape Town', 'Western Cape', -33.9750, 18.4620, 'https://adct.org.za/newlands/', NULL, NULL, 'st-bernard-catholic-church-newlands-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Newlands Methodist Church', 'Methodist', 'Newlands', 'Cape Town', 'Western Cape', -33.9750, 18.4630, NULL, NULL, NULL, 'newlands-methodist-church-cape-town', TRUE, FALSE);
