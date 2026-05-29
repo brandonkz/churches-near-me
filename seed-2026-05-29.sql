25c835219abc4a591ecbeb1c11c230cca2b8dc15
Brandon Katz <brandon@katz.co.za>
2026-05-29 03:13:42 +0200

    Auto: Add new churches

---
+++ seed-2026-05-29.sql
@@ -0,0 +1,23 @@
+-- ChurchesNearMe: 7 new churches added 2026-05-29
+-- Focus: Gauteng, KwaZulu-Natal, Western Cape
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St George''s Church Parktown', 'Anglican', '7 Sherborne Road, Parktown', 'Johannesburg', 'Gauteng', -26.1806, 28.0361, 'https://stgeorgesparktown.org/', '011 482 1606', 'secretary@stgeorges.org.za', 'st-georges-church-parktown-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Bryanston Methodist Church', 'Methodist', '115 Bryanston Drive, Bryanston', 'Johannesburg', 'Gauteng', -26.0529, 28.0186, 'https://www.bryanstonmethodist.org.za/', NULL, NULL, 'bryanston-methodist-church-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Manning Road Methodist Church', 'Methodist', 'Manning Road, Glenwood', 'Durban', 'KwaZulu-Natal', -29.8558, 30.9967, NULL, NULL, 'office@mrmc.co.za', 'manning-road-methodist-church-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Joseph''s Catholic Church Morningside', 'Catholic', '212 Florida Road, Morningside', 'Durban', 'KwaZulu-Natal', -29.8265, 31.0155, NULL, NULL, NULL, 'st-josephs-catholic-church-morningside-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Martini Lutheran Church Cape Town', 'Lutheran', '240 Long Street', 'Cape Town', 'Western Cape', -33.9239, 18.4149, 'https://www.lutherancape.org.za/cape-town-st-martini/', '021 423 5947', 'pastor@st-martini.co.za', 'st-martini-lutheran-church-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Andrew''s Presbyterian Church Cape Town', 'Presbyterian', 'Somerset Road', 'Cape Town', 'Western Cape', -33.9155, 18.4195, 'https://unitingpresbyterian.org/congregations/findcongregations/name/st-andrews-presbyterian-church-22/', '021 419 1059', 'info@standrews1828.co.za', 'st-andrews-presbyterian-church-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Rondebosch Presbyterian Church', 'Presbyterian', 'Rondebosch', 'Cape Town', 'Western Cape', -33.9626, 18.4765, NULL, NULL, NULL, 'rondebosch-presbyterian-church-cape-town', TRUE, FALSE);
