cdf532145cb0a02b8e76d9eaecbd8508876c4de6
Brandon Katz <brandon@katz.co.za>
2026-05-31 03:13:52 +0200

    Auto: Add new churches

---
+++ seed-2026-05-31.sql
@@ -0,0 +1,20 @@
+-- ChurchesNearMe: 6 new churches added 2026-05-31
+-- Focus: Gauteng, KwaZulu-Natal, Western Cape
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Andrew''s Anglican Church Kensington', 'Anglican', 'Kensington', 'Johannesburg', 'Gauteng', -26.1932, 28.1090, NULL, NULL, NULL, 'st-andrews-anglican-church-kensington-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Parkview Methodist Church', 'Methodist', '24 Tyrone Avenue, Parkview', 'Johannesburg', 'Gauteng', -26.1660, 28.0161, 'https://parkviewmethodist.co.za/', NULL, NULL, 'parkview-methodist-church-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Church of the Good Shepherd Protea', 'Anglican', 'Protea Road, Westville', 'Durban', 'KwaZulu-Natal', -29.8289, 30.9161, NULL, NULL, NULL, 'church-of-the-good-shepherd-protea-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Elizabeth''s Anglican Church Westville', 'Anglican', '45 Salisbury Avenue, Westville', 'Durban', 'KwaZulu-Natal', -29.8358, 30.9259, 'https://www.stelizabeths.co.za/', NULL, NULL, 'st-elizabeths-anglican-church-westville-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Margaret''s Anglican Church Fish Hoek', 'Anglican', 'Kommetjie Road, Fish Hoek', 'Cape Town', 'Western Cape', -34.1356, 18.4232, NULL, NULL, NULL, 'st-margarets-anglican-church-fish-hoek-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Wynberg Methodist Church', 'Methodist', 'Wynberg', 'Cape Town', 'Western Cape', -34.0065, 18.4662, NULL, NULL, NULL, 'wynberg-methodist-church-cape-town', TRUE, FALSE);
