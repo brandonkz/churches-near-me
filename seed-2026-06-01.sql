260a07c9a6d6721923bd58a4ee9e0d376b013210
Brandon Katz <brandon@katz.co.za>
2026-06-01 03:15:31 +0200

    Auto: Add new churches

---
+++ seed-2026-06-01.sql
@@ -0,0 +1,20 @@
+-- ChurchesNearMe: 6 new churches added 2026-06-01
+-- Focus: Gauteng, KwaZulu-Natal, Western Cape
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Martin de Porres Catholic Church Soweto', 'Catholic', 'Orlando West', 'Johannesburg', 'Gauteng', -26.2364, 27.9077, 'https://jesuitssouthern.africa/ministry/st-martin-de-porres-parish-orlando-west-soweto/', NULL, NULL, 'st-martin-de-porres-catholic-church-soweto', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Paul''s Anglican Church Parkhurst', 'Anglican', 'Parkhurst', 'Johannesburg', 'Gauteng', -26.1397, 28.0197, NULL, NULL, NULL, 'st-pauls-anglican-church-parkhurst-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Olav Church Durban', 'Anglican', 'Durban', 'Durban', 'KwaZulu-Natal', -29.8470, 31.0068, 'https://www.stolav.co.za/', NULL, NULL, 'st-olav-anglican-church-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Holy Trinity Catholic Parish Musgrave', 'Catholic', '210 Musgrave Road, Musgrave', 'Durban', 'KwaZulu-Natal', -29.8469, 31.0066, 'https://htcp.co.za/', '031 201 1288', 'secretary@htcp.co.za', 'holy-trinity-catholic-parish-musgrave-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Peter''s Anglican Church Mowbray', 'Anglican', '3 Church Street, Mowbray', 'Cape Town', 'Western Cape', -33.9479, 18.4767, 'https://stpetersmowbray.org.za/', NULL, NULL, 'st-peters-anglican-church-mowbray-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Constantia Catholic Church', 'Catholic', '4 Price Drive, Constantia', 'Cape Town', 'Western Cape', -34.0244, 18.4236, 'https://www.constantiacatholic.co.za/', '021 794 5185', 'constantia@adct.org.za', 'constantia-catholic-church-cape-town', TRUE, FALSE);
