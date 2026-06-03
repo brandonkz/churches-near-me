c9c30ee08f118e721b71526a3731409e38498d6a
Brandon Katz <brandon@katz.co.za>
2026-06-03 03:13:20 +0200

    Auto: Add new churches

---
+++ seed-2026-06-03.sql
@@ -0,0 +1,23 @@
+-- ChurchesNearMe: 7 new churches added 2026-06-03
+-- Focus: Gauteng, KwaZulu-Natal, Western Cape
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Saviour''s Anglican Church Claremont', 'Anglican', 'Claremont', 'Cape Town', 'Western Cape', -33.9840, 18.4630, 'https://stsaviours.weebly.com/', NULL, NULL, 'st-saviours-anglican-church-claremont-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Christ Church Umhlanga', 'Anglican', '57 Hambridge Avenue, Somerset Park, Umhlanga Rocks', 'Durban', 'KwaZulu-Natal', -29.7240, 31.0710, 'https://ccu.org.za/', '031 572 6542', 'admin@ccu.org.za', 'christ-church-umhlanga-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Empangeni Methodist Church', 'Methodist', 'Empangeni', 'Empangeni', 'KwaZulu-Natal', -28.7440, 31.8940, NULL, NULL, NULL, 'empangeni-methodist-church', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Pinelands Baptist Church', 'Baptist', 'Pinelands', 'Cape Town', 'Western Cape', -33.9340, 18.5050, 'https://pbc.org.za/', NULL, NULL, 'pinelands-baptist-church-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Common Ground Church Sea Point', 'Non-Denominational', 'Corner of Main Road and Marais Road, Sea Point', 'Cape Town', 'Western Cape', -33.9180, 18.3900, 'https://www.cgseapoint.co.za/', NULL, NULL, 'common-ground-church-sea-point-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Dunstan''s Cathedral Benoni', 'Anglican', 'Benoni', 'Benoni', 'Gauteng', -26.1880, 28.3200, 'https://www.stdunstanscathedral.org.za/', NULL, NULL, 'st-dunstans-cathedral-benoni', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Michael''s Anglican Church Bryanston', 'Anglican', '73 Mount Street, Bryanston', 'Johannesburg', 'Gauteng', -26.0540, 28.0220, 'http://stmichaelschurch.org.za', NULL, 'office@stmichaelschurch.co.za', 'st-michaels-anglican-church-bryanston-johannesburg', TRUE, FALSE);
