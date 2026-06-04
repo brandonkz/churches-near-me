5240f515cc9c6e2db9fe6821d759474fb5e0571f
Brandon Katz <brandon@katz.co.za>
2026-06-04 03:14:21 +0200

    Auto: Add new churches

---
+++ seed-2026-06-04.sql
@@ -0,0 +1,23 @@
+-- ChurchesNearMe: 7 new churches added 2026-06-04
+-- Focus: Gauteng, KwaZulu-Natal, Western Cape
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Common Ground Church Rondebosch', 'Non-Denominational', '23 Milner Road, Rondebosch', 'Cape Town', 'Western Cape', -33.9640, 18.4760, 'https://commongroundbosch.co.za/', NULL, NULL, 'common-ground-church-rondebosch-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('New Covenant Church Bryanston', 'Non-Denominational', '54 Hornbill Road, Bryanston', 'Johannesburg', 'Gauteng', -26.0520, 28.0100, 'https://www.nccb.co.za/', '011 704 3051', NULL, 'new-covenant-church-bryanston-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Gracepoint Methodist Church Glenferness', 'Methodist', '125 Duncan Road, Glenferness', 'Johannesburg', 'Gauteng', -26.0010, 28.0290, 'https://gracepoint.co.za/', '011 702 4600', 'info@gracepoint.co.za', 'gracepoint-methodist-church-glenferness-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Highway Christian Community', 'Pentecostal', '10 Harvey Road, Pinetown', 'Durban', 'KwaZulu-Natal', -29.8170, 30.8610, 'https://highway.org.za/', NULL, NULL, 'highway-christian-community-pinetown-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Agnes Anglican Church Kloof', 'Anglican', '53 Abelia Road, Kloof', 'Durban', 'KwaZulu-Natal', -29.7900, 30.8319, 'https://stagnes.org.za/', NULL, 'hello@stagnes.org.za', 'st-agnes-anglican-church-kloof-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Connect Church Meadowridge', 'Baptist', '136 Ladies Mile Road, Meadowridge', 'Cape Town', 'Western Cape', -34.0410, 18.4540, 'https://www.connectchurch.org.za/', '021 712 1218', 'office@connectchurch.org.za', 'connect-church-meadowridge-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Mark''s Methodist Church Edgemead', 'Methodist', 'Edgemead', 'Cape Town', 'Western Cape', -33.8730, 18.5410, 'https://www.stmarksedgemead.org.za/', NULL, NULL, 'st-marks-methodist-church-edgemead-cape-town', TRUE, FALSE);
