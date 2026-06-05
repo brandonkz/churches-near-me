c383e7c487c6289f8d34187a2f7e150981cdf279
Brandon Katz <brandon@katz.co.za>
2026-06-05 03:14:54 +0200

    Auto: Add new churches

---
+++ seed-2026-06-05.sql
@@ -0,0 +1,23 @@
+-- ChurchesNearMe: 7 new churches added 2026-06-05
+-- Focus: Gauteng, KwaZulu-Natal, Western Cape
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Columba''s Presbyterian Church Parkview', 'Presbyterian', '45 Lurgan Road, Parkview', 'Johannesburg', 'Gauteng', -26.1646, 28.0364, 'https://www.stcolumbaschurch.co.za/', '011 646 5420', 'admin@stcolumbas.org.za', 'st-columbas-presbyterian-church-parkview-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Francis of Assisi Anglican Church Parkview', 'Anglican', '46 Tyrone Avenue, Parkview', 'Johannesburg', 'Gauteng', -26.1608, 28.0354, 'https://stfrancisparkview.com/', '011 646 2660', NULL, 'st-francis-of-assisi-anglican-church-parkview-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Holy Trinity Catholic Church Braamfontein', 'Catholic', '16 Stiemens Street, Braamfontein', 'Johannesburg', 'Gauteng', -26.1935, 28.0395, 'https://trinityjhb.co.za/', NULL, NULL, 'holy-trinity-catholic-church-braamfontein-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Glenwood Community Church', 'Community Church', '115 Bulwer Road, Glenwood', 'Durban', 'KwaZulu-Natal', -29.8587, 30.9997, 'https://glenwoodchurch.org.za/', '031 202 7897', 'info@glenwoodchurch.org.za', 'glenwood-community-church-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('His Church Pinetown', 'Non-Denominational', '13 Drake Road, Cowies Hill Park', 'Durban', 'KwaZulu-Natal', -29.8210, 30.8750, 'https://www.hischurch.co.za/', '031 701 6211', 'hischurch@hischurch.co.za', 'his-church-pinetown-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Church of the Holy Spirit Kirstenhof', 'Anglican', '38 Raapkraal Road, Kirstenhof', 'Cape Town', 'Western Cape', -34.0710, 18.4400, 'https://chscapetown.org/', '021 701 3201', 'info@chscapetown.org', 'church-of-the-holy-spirit-kirstenhof-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Peter''s Church Mowbray', 'Anglican', '3 Church Street, Mowbray', 'Cape Town', 'Western Cape', -33.9479, 18.4745, 'https://stpetersmowbray.org.za/', NULL, NULL, 'st-peters-church-mowbray-cape-town', TRUE, FALSE);
