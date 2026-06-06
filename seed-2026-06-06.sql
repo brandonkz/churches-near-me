8ff4655bcdef34167ca4ef1e54ec8d81a69a2abd
Brandon Katz <brandon@katz.co.za>
2026-06-06 03:13:57 +0200

    Auto: Add new churches

---
+++ seed-2026-06-06.sql
@@ -0,0 +1,23 @@
+-- ChurchesNearMe: 7 new churches added 2026-06-06
+-- Focus: Gauteng, KwaZulu-Natal, Western Cape
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('newDAY Church Edenvale', 'Charismatic', '129 12th Avenue, Edenvale', 'Johannesburg', 'Gauteng', -26.1446, 28.1547, 'https://newday.co.za/', '011 452 4993', NULL, 'newday-church-edenvale-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('The Bridge Church Queenswood', 'Non-Denominational', 'Eduplex Auditorium, 1186 Dormer Avenue, Queenswood', 'Pretoria', 'Gauteng', -25.7357, 28.2479, 'https://www.thebridgechurch.co.za/', '082 900 7998', 'info@the-bridge.co.za', 'the-bridge-church-queenswood-pretoria', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Doxa Deo Midstream English', 'Doxa Deo', '10 Chorges Street, Midstream Estate, Centurion', 'Pretoria', 'Gauteng', -25.9140, 28.1980, 'https://www.doxadeo.org/midstreamenglish/', '010 516 5902', NULL, 'doxa-deo-midstream-centurion-pretoria', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Harvest Church Umhlanga', 'Church of the Nations', '1 Wincanton Way, Umhlanga', 'Durban', 'KwaZulu-Natal', -29.7248, 31.0715, 'https://harvestchurch.co.za/', '031 561 0338', 'admin@harvestchurch.co.za', 'harvest-church-umhlanga-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Grace Family Church Riverside', 'Non-Denominational', '63 Anthony Road, Durban North', 'Durban', 'KwaZulu-Natal', -29.7898, 31.0294, 'https://www.grace.org.za/', '031 563 1361', 'riverside@grace.org.za', 'grace-family-church-riverside-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Pinelands Methodist Church', 'Methodist', '2 Union Avenue, Pinelands', 'Cape Town', 'Western Cape', -33.9359, 18.5042, 'https://pinelandsmethodist.co.za/', NULL, NULL, 'pinelands-methodist-church-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Luke''s Anglican Church Diep River', 'Anglican', '7 Annandale Road, Diep River', 'Cape Town', 'Western Cape', -34.0302, 18.4665, 'https://stlukes.org.za/', NULL, NULL, 'st-lukes-anglican-church-diep-river-cape-town', TRUE, FALSE);
