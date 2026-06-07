1fd55ff08215aeeed4ca3c3825c32e85b2f70435
Brandon Katz <brandon@katz.co.za>
2026-06-07 03:14:07 +0200

    Auto: Add new churches

---
+++ seed-2026-06-07.sql
@@ -0,0 +1,23 @@
+-- ChurchesNearMe: 7 new churches added 2026-06-07
+-- Focus: Gauteng, KwaZulu-Natal, Western Cape
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Parkhurst Presbyterian Church', 'Presbyterian', 'Corner 4th Avenue and 12th Street, Parkhurst', 'Johannesburg', 'Gauteng', -26.1398, 28.0198, 'https://parkhurstpresby.co.za/', '011 788 2185', NULL, 'parkhurst-presbyterian-church-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Doxa Deo Brooklyn', 'Doxa Deo', '155 Dely Road, Waterkloof Glen', 'Pretoria', 'Gauteng', -25.7875, 28.2735, 'https://www.doxadeo.org/brooklyn/', NULL, NULL, 'doxa-deo-brooklyn-pretoria', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Patrick''s Catholic Church Benoni', 'Catholic', '41 Howard Avenue, Benoni', 'Benoni', 'Gauteng', -26.1886, 28.3208, 'https://stpatricksbenoni.co.za/', '011 422 1211', NULL, 'st-patricks-catholic-church-benoni', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Holy Trinity Anglican Church Musgrave', 'Anglican', '28 Berea Road, Musgrave', 'Durban', 'KwaZulu-Natal', -29.8498, 31.0008, 'https://holytrinitydbn.co.za/', NULL, NULL, 'holy-trinity-anglican-church-musgrave-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Glenashley Methodist Church', 'Methodist', '49 Ashley Avenue, Glenashley', 'Durban', 'KwaZulu-Natal', -29.7548, 31.0587, 'https://glenashleymethodist.co.za/', NULL, NULL, 'glenashley-methodist-church-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Claremont Congregational Church', 'Congregational', '222 Main Road, Claremont', 'Cape Town', 'Western Cape', -33.9826, 18.4661, 'https://claremontcongregationalchurch.co.za/', NULL, NULL, 'claremont-congregational-church-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Paul''s Catholic Church Table View', 'Catholic', '11 Janssens Avenue, Table View', 'Cape Town', 'Western Cape', -33.8247, 18.4892, 'https://stpaulscatholicchurch.co.za/', NULL, NULL, 'st-pauls-catholic-church-table-view-cape-town', TRUE, FALSE);
