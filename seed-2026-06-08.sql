d504ba1ac477b7dd9492f40848ecadd69b851ee7
Brandon Katz <brandon@katz.co.za>
2026-06-08 03:13:43 +0200

    Auto: Add new churches

---
+++ seed-2026-06-08.sql
@@ -0,0 +1,23 @@
+-- ChurchesNearMe: 7 new churches added 2026-06-08
+-- Focus: Gauteng, KwaZulu-Natal, Western Cape
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Rosebank Union Church', 'Evangelical', '38 St Andrews Road, Hurlingham', 'Johannesburg', 'Gauteng', -26.1048, 28.0181, 'https://ruc.org.za/', '011 783 6211', NULL, 'rosebank-union-church-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Bryanston Methodist Church', 'Methodist', '115 Grosvenor Road, Bryanston', 'Johannesburg', 'Gauteng', -26.0524, 28.0242, 'https://bmc.org.za/', '011 706 0330', NULL, 'bryanston-methodist-church-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('NG Kerk Waterkloof', 'Dutch Reformed', '314 Albert Street, Waterkloof', 'Pretoria', 'Gauteng', -25.7798, 28.2358, 'https://www.ngwaterkloof.co.za/', NULL, NULL, 'ng-kerk-waterkloof-pretoria', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Martin''s Anglican Church Durban North', 'Anglican', '12 Church Road, Durban North', 'Durban', 'KwaZulu-Natal', -29.7857, 31.0395, 'https://www.stmartins.co.za/', NULL, NULL, 'st-martins-anglican-church-durban-north', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Liberty Church Umhlanga', 'Charismatic', '3 Tetford Circle, Umhlanga Ridge', 'Durban', 'KwaZulu-Natal', -29.7258, 31.0697, 'https://libertychurch.co.za/', NULL, NULL, 'liberty-church-umhlanga-durban', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Common Ground Church Rondebosch', 'Evangelical', '23 Milner Road, Rondebosch', 'Cape Town', 'Western Cape', -33.9637, 18.4762, 'https://commonground.co.za/', NULL, NULL, 'common-ground-church-rondebosch-cape-town', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St James Church Kenilworth', 'Anglican', '114 Third Avenue, Kenilworth', 'Cape Town', 'Western Cape', -33.9971, 18.4728, 'https://stjames.org.za/', '021 671 7241', NULL, 'st-james-church-kenilworth-cape-town', TRUE, FALSE);
