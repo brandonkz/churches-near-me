532b091e0883ec6362012cff01490e4dca01f55b
Brandon Katz <brandon@katz.co.za>
2026-06-12 03:12:00 +0200

    Auto: Add 8 new churches (Gauteng/KZN/WC) 2026-06-12

---
+++ seed-2026-06-12.sql
@@ -0,0 +1,26 @@
+-- ChurchesNearMe: 8 new churches added 2026-06-12
+-- Focus: Gauteng, KwaZulu-Natal, Western Cape
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Northgate Community Church', 'Community Church', '2 Northridge Road, Northgate', 'Johannesburg', 'Gauteng', -26.1068, 27.9528, 'https://www.northgatechurch.co.za/', '011 794 6112', 'info@northgatechurch.co.za', 'northgate-community-church-johannesburg', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Edenvale Dutch Reformed Church', 'Dutch Reformed', '1 Hendrik Verwoerd Drive, Edenvale', 'Edenvale', 'Gauteng', -26.1427, 28.1583, NULL, '011 453 1322', NULL, 'edenvale-dutch-reformed-church', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Centurion Baptist Church', 'Baptist', '10 Akkerboom Road, Eldoraigne', 'Centurion', 'Gauteng', -25.8736, 28.1347, 'https://www.centurionbaptist.org.za/', '012 654 0188', 'office@centurionbaptist.org.za', 'centurion-baptist-church', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St Andrew''s Presbyterian Church Bloemfontein Rd Westville', 'Presbyterian', '36 Jan Hofmeyr Road, Westville', 'Durban', 'KwaZulu-Natal', -29.8362, 30.9277, NULL, '031 266 1001', NULL, 'st-andrews-presbyterian-church-westville', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Empangeni Baptist Church', 'Baptist', '10 Pearce Street, Empangeni', 'Empangeni', 'KwaZulu-Natal', -28.7500, 31.9000, NULL, '035 772 1459', NULL, 'empangeni-baptist-church', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('St John''s Anglican Church Pinetown', 'Anglican', '12 Crompton Street, Pinetown', 'Durban', 'KwaZulu-Natal', -29.8151, 30.8615, 'https://www.stjohnspinetown.org.za/', '031 702 3732', NULL, 'st-johns-anglican-church-pinetown', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Stellenbosch Baptist Church', 'Baptist', '13 Van Riebeeck Street, Stellenbosch Central', 'Stellenbosch', 'Western Cape', -33.9343, 18.8612, 'https://www.stellenboschbaptist.org.za/', '021 883 3460', 'office@stellenboschbaptist.org.za', 'stellenbosch-baptist-church', TRUE, FALSE);
+
+INSERT INTO churches (name, denomination, address, city, province, lat, lng, website, phone, email, slug, verified, featured) VALUES
+('Parow Methodist Church', 'Methodist', '3 Liesbeeck Road, Parow', 'Cape Town', 'Western Cape', -33.9003, 18.5917, NULL, '021 930 1624', NULL, 'parow-methodist-church-cape-town', TRUE, FALSE);
